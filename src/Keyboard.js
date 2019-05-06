1845674685b1e9189ddc8772bf4bd1432d5e489f
cvensand <cvensand@umich.edu>
2019-05-06 12:48:12 -0400

    init keys

--- src/Keyboard.js
+++ src/Keyboard.js
@@ -2,14 +2,24 @@ import React from 'react';
 import Key from './Key.js';
 
 class Keyboard extends React.Component {
-    render() {
-        const letters = ['A','S','D','F','G','H','J','K','L',';'];
-        var keys = letters.map(function(letter) {
-                        return <Key key={letter} letter={letter}/>;
-                    });
+    constructor(props) {
+        super(props);
+        this.bottomLetters = ['A','S','D','F','G','H','J','K','L',';'];
+        this.topLetters = ['W','E','T','Y','U','O','P'];
+        this.bottomKeys = this.bottomLetters.map(function(letter) {
+                            return <Key key={letter} letter={letter} />;
+                        });
+        this.topKeys = this.topLetters.map(function(letter) {
+                            return <Key key={letter} letter={letter} />;
+                        });
+    }
 
+    render() {
         return (
-            <div className="Keyboard">{ keys }</div>
+            <div className="Keyboard">
+                { this.bottomKeys }
+                { this.topKeys }
+            </div>
         );
     }
 }
