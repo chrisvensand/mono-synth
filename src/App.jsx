1d2505b19de921173f6b258954e35b0e05b2b70a
cvensand <cvensand@umich.edu>
2019-05-09 15:22:59 -0700

    Remove header component

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Keyboard from './Keyboard.jsx';
+import './index.css'
+
+class App extends React.Component {
+    render() {
+		return (
+            <Keyboard />
+		);
+	}
+}
+
+export default App;
