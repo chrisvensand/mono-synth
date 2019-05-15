0fd4060b2e329d2f8d44f11ac8855b32b2001e39
cvensand <cvensand@umich.edu>
2019-05-15 13:42:41 -0700

    style component

---
+++ src/Presets.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import './index.css';
+
+export default class Presets extends React.Component {
+    render() {
+        return (
+            <div className="presets">          
+            </div>
+        );
+    }
+}
