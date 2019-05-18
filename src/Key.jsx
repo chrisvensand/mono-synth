1e18e42969ca837e1228750b989fe0a70ff02087
cvensand <cvensand@umich.edu>
2019-05-17 18:39:49 -0700

    add conditional operator

--- src/Key.jsx
+++ src/Key.jsx
@@ -10,7 +10,7 @@ export default class Key extends React.Component {
     }
 
     componentDidUpdate(prevProps) {
-        // Check for key press
+        // Check for a change in the pressed state of the key
         if (this.props.isPressed !== prevProps.isPressed) {
             if (this.props.isPressed) {
                 this.state.synth.triggerAttack(this.props.frequency);
@@ -18,17 +18,18 @@ export default class Key extends React.Component {
                 this.state.synth.triggerRelease();
             }
         }
+        // Check for a preset sound change
+        if (this.props.settings !== prevProps.settings) {
+            this.setState({synth: new Tone.MonoSynth(this.props.settings).toMaster()});
+        }
     }
 
     render() {
-        var name = "key";
-        if (this.props.isPressed) {
-            name = "pressedKey";
-        }
+        let name = this.props.isPressed ? "pressed-key" : "key"
 
         return (
-            <button className={name}
-                    id={this.props.letter}
+            <button id={this.props.letter}
+                    className={name}
                     onMouseDown={this.props.handleMouseDown}
                     onMouseUp={this.props.handleMouseUp}>
                 {this.props.letter}
