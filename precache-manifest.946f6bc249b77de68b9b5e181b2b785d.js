20670ba4bfcabd7b2916db87e094a40c86e4a50b
cvensand <cvensand@umich.edu>
2019-05-21 13:57:08 -0700

    Updates

---
+++ precache-manifest.946f6bc249b77de68b9b5e181b2b785d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "93df9d4cf00163a570868abbc25f929d",
+    "url": "/monosynth/index.html"
+  },
+  {
+    "revision": "68e95b1cc8ac6da3bfd2",
+    "url": "/monosynth/static/css/main.12f36bd2.chunk.css"
+  },
+  {
+    "revision": "da193e3445521ab08de2",
+    "url": "/monosynth/static/js/2.7c067087.chunk.js"
+  },
+  {
+    "revision": "68e95b1cc8ac6da3bfd2",
+    "url": "/monosynth/static/js/main.87ecfb0b.chunk.js"
+  },
+  {
+    "revision": "5f07c335ddd962479d1c",
+    "url": "/monosynth/static/js/runtime~main.013aee5e.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.0/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.4927c897035635d442e8ec2333dccada.js"
+  "/monosynth/precache-manifest.946f6bc249b77de68b9b5e181b2b785d.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/monosynth/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
